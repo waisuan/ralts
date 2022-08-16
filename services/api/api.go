387fc58d1ffcbb9e8292227d407a79e1c3e85160
Evan Sia <ws-91@hotmail.com>
2022-08-16 13:20:44 +0800

    WIP

--- services/api/api.go
+++ services/api/api.go
@@ -3,15 +3,30 @@ package api
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"some-api/services/location"
+	"some-api/utils/db"
 )
 
-func NewApi() *echo.Echo{
-	e := echo.New()
-	e.GET("/location/:id", getLocation)
-	return e
+type Api struct {
+	db db.DataStore
+	echo *echo.Echo
 }
 
-func getLocation(c echo.Context) error {
+func NewApi(dbClient db.DataStore) *Api {
+	e := echo.New()
+	a := &Api{
+		db: dbClient,
+		echo: e,
+	}
+	e.GET("/location/:id", a.getLocation)
+	return a
+}
+
+func (a *Api) getLocation(c echo.Context) error {
 	personId := c.Param("id")
-	return c.String(http.StatusOK, personId)
+	out, err := location.Get(a.db, personId)
+	if err != nil {
+		c.Error(err)
+	}
+	return c.JSON(http.StatusOK, out)
 }
--- services/api/api_test.go
+++ services/api/api_test.go
@@ -1,1 +1,49 @@
 package api
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"net/http"
+	"net/http/httptest"
+	"some-api/utils/db"
+	"strings"
+	"testing"
+)
+
+func TestApi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "API test suite")
+}
+
+var _ = Describe("getLocation", func() {
+	Context("Happy", func() {
+		It("Runs", func() {
+			mockDB := &db.MockDatabaseClient{
+				GetByPkFn: func(pk string) (map[string]types.AttributeValue, error) {
+					return map[string]types.AttributeValue{
+						"pk":        &types.AttributeValueMemberS{Value: "1234"},
+						"personID":  &types.AttributeValueMemberS{Value: "1234"},
+						"latitude":  &types.AttributeValueMemberS{Value: "1.12"},
+						"longitude": &types.AttributeValueMemberS{Value: "0.9999"},
+					}, nil
+				},
+			}
+			a := NewApi(mockDB)
+
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := a.echo.NewContext(req, rec)
+			c.SetPath("/location/:id")
+			c.SetParamNames("id")
+			c.SetParamValues("Evan")
+
+			err := a.getLocation(c)
+			Expect(err).To(BeNil())
+			Expect(rec.Code).To(Equal(http.StatusOK))
+			Expect(strings.TrimSuffix(rec.Body.String(), "\n")).To(
+				Equal(`{"Pk":"1234","PersonID":"1234","Latitude":"1.12","Longitude":"0.9999"}`),
+			)
+		})
+	})
+})
